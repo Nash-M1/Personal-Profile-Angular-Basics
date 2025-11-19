7329fb7dceef737d04ce88c4cb0bb1072ad9adf5
Nash-M1 <knashpmallari1@gmail.com>
2025-11-19 16:28:07 +0800

    Initial commit

---
+++ src/app/certifications/certifications.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certifications',
+  standalone: true,
+  templateUrl: './certifications.html',
+  styleUrl: './certifications.css',
+})
+export class Certifications {
+
+}
